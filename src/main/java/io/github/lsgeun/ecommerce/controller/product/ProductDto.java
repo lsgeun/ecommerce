6351042f21ce5a4d3eef8031d68e3ef68f4b12ff
Sanggeun Lee <zlwkr1974@gmail.com>
2026-07-01 15:23:00 +0900

    feature: ProductDto Read, Create Dto를 레코드 클래스로 바꿈

--- src/main/java/io/github/lsgeun/ecommerce/controller/product/ProductDto.java
+++ src/main/java/io/github/lsgeun/ecommerce/controller/product/ProductDto.java
@@ -1,32 +1,42 @@
 package io.github.lsgeun.ecommerce.controller.product;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import io.github.lsgeun.ecommerce.domain.product.ProductStatus;
 import lombok.Builder;
 import lombok.Getter;
 
 public class ProductDto {
-    @Builder
-    @Getter
-    public static class ReadResponse {
-        Long id;
-        String number;
-        String name;
-        int price;
-        int stock;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    public record ReadResponse (
+        Long id,
+        String number,
+        String name,
+        int price,
+        int stock,
+        ProductStatus status
+    ) {
+        @Builder(builderMethodName = "builder")
+        public ReadResponse {}
     }
 
-    public static class CreateRequest {
-        String number;
-        String name;
-        int price;
-        int stock;
-    }
-    @Builder
-    @Getter
-    public static class CreateResponse {
-        Long id;
-        String number;
-        String name;
-        int price;
-        int stock;
+    public record CreateRequest (
+        String number,
+        String name,
+        int price,
+        int stock,
+        ProductStatus status
+    ) {}
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    public record CreateResponse (
+        Long id,
+        String number,
+        String name,
+        int price,
+        int stock,
+        ProductStatus status
+    ) {
+        @Builder(builderMethodName = "builder")
+        public CreateResponse {}
     }
 }
