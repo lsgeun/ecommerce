e379794af898484a1c39f4ca9666516fbf521d5d
Sanggeun Lee <zlwkr1974@gmail.com>
2026-07-01 17:18:00 +0900

    feature: Product 도메인을 빌더를 포함한 팩토리 패턴으로 객체를 생성하도록 함

--- src/main/java/io/github/lsgeun/ecommerce/domain/product/Product.java
+++ src/main/java/io/github/lsgeun/ecommerce/domain/product/Product.java
@@ -2,6 +2,7 @@ package io.github.lsgeun.ecommerce.domain.product;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -32,4 +33,27 @@ public class Product {
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private ProductStatus status;
+
+    @Builder(access = AccessLevel.PRIVATE, builderMethodName = "builder")
+    private Product(Long id, String number, String name, long price, int stock, ProductStatus status) {
+        this.id = id;
+        this.number = number;
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+        this.status = status;
+    }
+
+    public static Product create(String number, String name, long price, int stock, ProductStatus status) {
+        validateNumber(number);
+        validateName(name);
+        validatePrice(price);
+        validateStock(stock);
+        validateStatus(status);
+
+        return Product.builder()
+            .id(null)
+            .number(number).name(name).price(price).stock(stock).status(status)
+            .build();
+    }
 }
