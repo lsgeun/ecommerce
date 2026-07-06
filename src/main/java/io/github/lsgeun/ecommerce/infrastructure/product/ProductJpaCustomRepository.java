03c4000cd1333a9da4bee9030d0c04fed3e0198b
Sanggeun Lee <zlwkr1974@gmail.com>
2026-07-06 14:03:35 +0900

    style: 자동으로 정렬되는 import 문 순서에 맞게 QProduct import문 위치 변경

--- src/main/java/io/github/lsgeun/ecommerce/infrastructure/product/ProductJpaCustomRepository.java
+++ src/main/java/io/github/lsgeun/ecommerce/infrastructure/product/ProductJpaCustomRepository.java
@@ -1,7 +1,7 @@
 package io.github.lsgeun.ecommerce.infrastructure.product;
 
-import io.github.lsgeun.ecommerce.domain.product.QProduct;
 import io.github.lsgeun.ecommerce.domain.product.Product;
+import io.github.lsgeun.ecommerce.domain.product.QProduct;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.hibernate.Session;
