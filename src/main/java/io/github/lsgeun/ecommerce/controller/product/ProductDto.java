9b6c3deba6d75cbd77a502dfce2e857029599ea4
Sanggeun Lee <zlwkr1974@gmail.com>
2026-07-08 16:49:47 +0900

    feature: update, delete dto 추가, 검증 어노테이션 추가
    update dto는 patch, put 메서드에 대응하는 dto로 나누어야 한다.
    전자의 경우 기본 타입 대신 래퍼 객체를 사용해야 하고, @NotNull을 빼야 한다. 일부 속성만 수정하는 것이기 때문이다.
    
    페이지네이션 dto나 검색 dto도 만들어야 한다. 페이지나 검색 기능을 지원하기 위해서 말이다.

--- src/main/java/io/github/lsgeun/ecommerce/controller/product/ProductDto.java
+++ src/main/java/io/github/lsgeun/ecommerce/controller/product/ProductDto.java
@@ -2,6 +2,9 @@ package io.github.lsgeun.ecommerce.controller.product;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import io.github.lsgeun.ecommerce.domain.product.ProductStatus;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 
 public class ProductDto {
@@ -24,10 +27,21 @@ public class ProductDto {
 
     // Create
     public record CreateRequest(
+        @NotNull(message = "상품 번호는 필수입니다.")
+        @Size(min = 2, max = 50, message = "상품 번호는 2자 이상 50자 이하이어야 합니다.")
         String number,
+
+        @NotNull(message = "상품 이름은 필수입니다.")
+        @Size(min = 2, max = 50, message = "상품 이름은 2자 이상 50자 이하이어야 합니다.")
         String name,
+
+        @Min(value = 0, message = "상품 가격은 0 이상이어야 합니다.")
         int price,
+
+        @Min(value = 0, message = "상품 재고는 0 이상이어야 합니다.")
         int stock,
+
+        @NotNull(message = "상품 상태는 필수입니다.")
         ProductStatus status
     ) {}
 
@@ -42,6 +56,57 @@ public class ProductDto {
 
         @Builder
         public CreateResponse {
+            // 내부 레코드에서 Lombok @Builder를 사용하기 위한 빈 컴팩트 생성자
+        }
+    }
+
+    // Update
+    public record UpdateRequest(
+        @NotNull(message = "상품 번호는 필수입니다.")
+        @Size(min = 2, max = 50, message = "상품 번호는 2자 이상 50자 이하이어야 합니다.")
+        String number,
+
+        @NotNull(message = "상품 이름은 필수입니다.")
+        @Size(min = 2, max = 50, message = "상품 이름은 2자 이상 50자 이하이어야 합니다.")
+        String name,
+
+        @Min(value = 0, message = "상품 가격은 0 이상이어야 합니다.")
+        int price,
+
+        @Min(value = 0, message = "상품 재고는 0 이상이어야 합니다.")
+        int stock,
+
+        @NotNull(message = "상품 상태는 필수입니다.")
+        ProductStatus status
+    ) {}
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    public record UpdateResponse(
+        String number,
+        String name,
+        int price,
+        int stock,
+        ProductStatus status
+    ) {
+
+        @Builder
+        public UpdateResponse {
+            // 내부 레코드에서 Lombok @Builder를 사용하기 위한 빈 컴팩트 생성자
+        }
+    }
+
+    // DELETE
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    public record DeleteResponse(
+        String number,
+        String name,
+        int price,
+        int stock,
+        ProductStatus status
+    ) {
+
+        @Builder
+        public DeleteResponse {
             // 내부 레코드에서 Lombok @Builder를 사용하기 위한 빈 컴팩트 생성자
         }
     }
