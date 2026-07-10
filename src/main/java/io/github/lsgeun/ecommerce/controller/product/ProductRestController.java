7e3690e9c31a03f60560c756aa8273a2bdd07b99
Sanggeun Lee <zlwkr1974@gmail.com>
2026-07-10 10:30:05 +0900

    fix: 컨트롤러에서 직접 검증할 수 있도록 @Validated 추가

--- src/main/java/io/github/lsgeun/ecommerce/controller/product/ProductRestController.java
+++ src/main/java/io/github/lsgeun/ecommerce/controller/product/ProductRestController.java
@@ -6,6 +6,7 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Validated
 @RequiredArgsConstructor
 @RequestMapping(path = "/product")
 @RestController
