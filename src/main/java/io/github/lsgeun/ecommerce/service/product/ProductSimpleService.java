9593a573ab8454ad797a6ad7d8d037907debe9dc
Sanggeun Lee <zlwkr1974@gmail.com>
2026-07-10 10:33:30 +0900

    feature: product 서비스 메서드 number 파라미터에 검증 어노테이션 쓰지 않고, 도메인 검증 메서드를 사용하도록 변경

--- src/main/java/io/github/lsgeun/ecommerce/service/product/ProductSimpleService.java
+++ src/main/java/io/github/lsgeun/ecommerce/service/product/ProductSimpleService.java
@@ -5,8 +5,6 @@ import io.github.lsgeun.ecommerce.controller.product.ProductDtoMapper;
 import io.github.lsgeun.ecommerce.domain.product.Product;
 import io.github.lsgeun.ecommerce.domain.product.ProductRepository;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,8 +20,6 @@ public class ProductSimpleService {
 
     @Transactional(readOnly = true)
     public ProductDto.ReadResponse getProduct(
-        @NotNull(message = "상품 번호는 필수입니다.")
-        @Size(min = 2, max = 50, message = "상품 번호는 2자 이상 50자 이하이어야 합니다.")
         String number
     ) {
         Product.validateNumber(number);
@@ -61,10 +57,10 @@ public class ProductSimpleService {
 
     @Transactional
     public ProductDto.DeleteResponse deleteProduct(
-        @NotNull(message = "상품 번호는 필수입니다.")
-        @Size(min = 2, max = 50, message = "상품 번호는 2자 이상 50자 이하이어야 합니다.")
         String number
     ) {
+        Product.validateNumber(number);
+
         Product product = productRepository.deleteByNumber(number);
 
         return productDtoMapper.toDeleteResponse(product);
