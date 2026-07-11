52775b10e5604d8198db6377ed63d3e36fe58976
Sanggeun Lee <zlwkr1974@gmail.com>
2026-07-11 15:57:16 +0900

    refactor: 사용하지 않는 상품 리포지토리 추상 메서드 삭제

--- src/main/java/io/github/lsgeun/ecommerce/infrastructure/product/ProductJpaRepository.java
+++ src/main/java/io/github/lsgeun/ecommerce/infrastructure/product/ProductJpaRepository.java
@@ -3,13 +3,7 @@ package io.github.lsgeun.ecommerce.infrastructure.product;
 import io.github.lsgeun.ecommerce.domain.product.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface ProductJpaRepository extends JpaRepository<Product, Long>, ProductJpaCustomRepository {
-
-    Optional<Product> findByNumber(String number);
-
-    void deleteByNumber(String number);
 
     boolean existsByNumber(String number);
 }
