c9d090733790c6cbc62f3472429eaace8f4592d1
Sanggeun Lee <zlwkr1974@gmail.com>
2026-07-28 14:33:07 +0900

    refactor: 상품 도메인 가독성 향상
    세로로 너무 긴 코드를 가로로 적당히 펼침.
    isValid 불리언 변수를 사용하는 대신 if 문에 직접 불리언 로직 작성.
    InvalidDomainFieldException에 null를 직접 전달하기 보다 변수 사용

--- src/main/java/io/github/lsgeun/ecommerce/domain/product/Product.java
+++ src/main/java/io/github/lsgeun/ecommerce/domain/product/Product.java
@@ -54,12 +54,7 @@ public class Product {
 
     @Builder(access = AccessLevel.PUBLIC)
     private Product(
-        Long id,
-        String number,
-        String name,
-        long price,
-        int stock,
-        ProductStatus status
+        Long id, String number, String name, long price, int stock, ProductStatus status
     ) {
         validateNumber(number);
         validateName(name);
@@ -90,21 +85,13 @@ public class Product {
     public static void validateNumber(String number) {
         if (number == null) {
             throw new InvalidDomainFieldException(
-                Product.class,
-                "number",
-                null,
-                "상품 번호는 필수입니다."
+                Product.class, "number", number, "상품 번호는 필수입니다."
             );
         }
 
-        boolean isValid =
-            (2 <= number.length() && number.length() <= 50);
-        if (!isValid) {
+        if (!(2 <= number.length() && number.length() <= 50)) {
             throw new InvalidDomainFieldException(
-                Product.class,
-                "number",
-                number,
-                "상품 번호는 2자 이상 50자 이하이어야 합니다."
+                Product.class, "number", number, "상품 번호는 2자 이상 50자 이하이어야 합니다."
             );
         }
     }
@@ -112,45 +99,29 @@ public class Product {
     public static void validateName(String name) {
         if (name == null) {
             throw new InvalidDomainFieldException(
-                Product.class,
-                "name",
-                null,
-                "상품 이름은 필수입니다."
+                Product.class, "name", name, "상품 이름은 필수입니다."
             );
         }
 
-        boolean isValid =
-            (2 <= name.length() && name.length() <= 50);
-        if (!isValid) {
+        if (!(2 <= name.length() && name.length() <= 50)) {
             throw new InvalidDomainFieldException(
-                Product.class,
-                "name",
-                name,
-                "상품 이름은 2자 이상 50자 이하이어야 합니다."
+                Product.class, "name", name, "상품 이름은 2자 이상 50자 이하이어야 합니다."
             );
         }
     }
 
     public static void validatePrice(long price) {
-        boolean isValid = (price >= 0);
-        if (!isValid) {
+        if (price < 0) {
             throw new InvalidDomainFieldException(
-                Product.class,
-                "price",
-                price,
-                "상품 가격은 0 이상이어야 합니다."
+                Product.class, "price", price, "상품 가격은 0 이상이어야 합니다."
             );
         }
     }
 
     public static void validateStock(int stock) {
-        boolean isValid = (stock >= 0);
-        if (!isValid) {
+        if (stock < 0) {
             throw new InvalidDomainFieldException(
-                Product.class,
-                "stock",
-                stock,
-                "상품 재고는 0 이상이어야 합니다."
+                Product.class, "stock", stock, "상품 재고는 0 이상이어야 합니다."
             );
         }
     }
@@ -158,10 +129,7 @@ public class Product {
     public static void validateStatus(ProductStatus status) {
         if (status == null) {
             throw new InvalidDomainFieldException(
-                Product.class,
-                "status",
-                null,
-                "상품 상태는 필수입니다."
+                Product.class, "status", status, "상품 상태는 필수입니다."
             );
         }
     }
