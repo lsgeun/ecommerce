b4ac37ba74af1802e71463d63191f5f01f7fe848
Sanggeun Lee <zlwkr1974@gmail.com>
2026-08-06 16:33:44 +0900

    chore: ci가 작동하는지, 슬랙 알림 오는지 테스트

--- src/main/java/io/github/lsgeun/ecommerce/product/presentation/ProductRestController.java
+++ src/main/java/io/github/lsgeun/ecommerce/product/presentation/ProductRestController.java
@@ -23,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ProductRestController {
 
+
     private final ProductSimpleService productSimpleService;
     private final ProductDtoMapper productDtoMapper;
 
