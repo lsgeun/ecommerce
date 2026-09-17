aa053af7d38c2486bc7408864a8b852cc0ef08cd
Sanggeun Lee <lsgeun.me@gmail.com>
2026-09-17 14:33:16 +0900

    test: gradle 테스트 작업을 4개로 분리 후 순서 지정

---
+++ backend/spring-core-api/src/test/java/io/github/lsgeun/ecommerce/support/annotation/SliceTest.java
@@ -0,0 +1,13 @@
+package io.github.lsgeun.ecommerce.support.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.junit.jupiter.api.Tag;
+
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Tag("slice")
+public @interface SliceTest {
+}
