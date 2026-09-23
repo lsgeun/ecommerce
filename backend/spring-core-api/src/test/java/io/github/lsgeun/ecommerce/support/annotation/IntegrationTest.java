493d71376db2cd38d8c415b534fab3e28435f014
Sanggeun Lee <lsgeun.me@gmail.com>
2026-09-23 17:18:10 +0900

    test: 커스텀 어노테이션 구조 변경

--- backend/spring-core-api/src/test/java/io/github/lsgeun/ecommerce/support/annotation/IntegrationTest.java
+++ backend/spring-core-api/src/test/java/io/github/lsgeun/ecommerce/support/annotation/IntegrationTest.java
@@ -5,11 +5,13 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import org.junit.jupiter.api.Tag;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
 @Target({ElementType.TYPE, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 @Tag("integration")
+@SpringBootTest
 public @interface IntegrationTest {
 }
--- backend/spring-core-api/src/test/java/io/github/lsgeun/ecommerce/support/annotation/UnitTest.java
+++ backend/spring-core-api/src/test/java/io/github/lsgeun/ecommerce/support/annotation/UnitTest.java
@@ -1,14 +1,11 @@
 package io.github.lsgeun.ecommerce.support.annotation;
 
 import java.lang.annotation.ElementType;
-import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import org.junit.jupiter.api.Tag;
-import org.springframework.test.context.ActiveProfiles;
 
-@ActiveProfiles("test")
 @Target({ElementType.TYPE, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 @Tag("unit")
